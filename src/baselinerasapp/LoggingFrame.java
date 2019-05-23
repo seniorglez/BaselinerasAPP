6c790bd6a3c71815f3ee47a89c880d7048071266
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2019-05-23 12:10:50 +0200

    The logging frame is not that ugly rigth now, but is not finished yet

--- src/baselinerasapp/LoggingFrame.java
+++ src/baselinerasapp/LoggingFrame.java
@@ -8,6 +8,7 @@ package baselinerasapp;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -23,7 +24,7 @@ import javax.swing.JTextField;
 public class LoggingFrame extends JFrame implements ActionListener{
 
     //variable
-    private JPanel panBox, panelSuperior, panelInferior;
+    private JPanel panBox, panelSuperior, panelInferior, panelBoton;
     private JLabel etiqueta1, etiqueta2, etiqueta3;
     private JTextField cajaTexto;
     private JPasswordField passb;
@@ -42,47 +43,59 @@ public class LoggingFrame extends JFrame implements ActionListener{
 
         panBox = new JPanel();
         panBox.setLayout(new BoxLayout(panBox, BoxLayout.Y_AXIS));
-        etiqueta1 = new JLabel("LOGGING");
+        etiqueta1 = new JLabel("Introduce tus credenciales");
         construyePanelSuperior();
         construyePanelInferior();
+        construyePanelBoton();
         panBox.add(etiqueta1);
         panBox.add(panelSuperior);
-        
         panBox.add(panelInferior);
-        summit=new JButton("GO");
-        summit.addActionListener(this);
-        panBox.add(summit);
+        panBox.add(panelBoton);
+        
         this.getContentPane().add(panBox);
         this.setSize(new Dimension(300, 125));
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
+        this.setResizable(false);
 
     }
 
     public void construyePanelSuperior() {
         panelSuperior = new JPanel();
-        etiqueta2 = new JLabel("Usuario");
+        etiqueta2 = new JLabel("Usuario           ");
         etiqueta2.getSize(new Dimension(100,20));
         cajaTexto = new JTextField(10);
         panelSuperior.setLayout(new BoxLayout(panelSuperior, BoxLayout.X_AXIS));
         panelSuperior.add(etiqueta2);
+        panelSuperior.add(Box.createHorizontalGlue());
         panelSuperior.add(cajaTexto);
     }
 
     public void construyePanelInferior() {
         panelInferior = new JPanel();
-        etiqueta3 = new JLabel("contraseña");
+        etiqueta3 = new JLabel("contraseña    ");
         etiqueta3.getSize(new Dimension(100,20));
         passb = new JPasswordField(10);
         panelInferior.setLayout(new BoxLayout(panelInferior, BoxLayout.X_AXIS));
         panelInferior.add(etiqueta3);
+        panelInferior.add(Box.createHorizontalGlue());
         panelInferior.add(passb);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void construyePanelBoton() {
+        panelBoton=new JPanel();
+        panelBoton.setLayout(new BoxLayout(panelBoton, BoxLayout.X_AXIS));
+        summit=new JButton("conectar");
+        summit.addActionListener(this);
+        panelBoton.add(Box.createHorizontalGlue());
+        panelBoton.add(summit);
+        panelBoton.add(Box.createHorizontalGlue());
     }
 
 }
