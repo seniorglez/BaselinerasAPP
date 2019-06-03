bb76691a8403f7287b37807703db2a71a333f1b4
Edgar Lopez <edoardoelh@gmail.com>
2019-06-03 19:34:03 +0200

    Creacion de clases Connection, Delete, Select, Update

---
+++ src/baselinerasapp/Model/Delete.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baselinerasapp.Model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author EdoardoELH
+ */
+public class Delete {
+    ///Atributos///
+    private static Delete delete;
+    private ResultSet rs;
+    private java.sql.Connection con;
+    private Object[] fila;
+    private int numCol;
+
+    ///Constructores///
+    private Delete() {
+
+    }
+
+    ///Metodos///
+    public static Delete getConnection() {
+        if (delete == null) {
+            delete = new Delete();
+        }
+        return delete;
+    }
+
+    public int executeUpdate(String sql) {
+        con = Connection.getConnection().getC();
+        try (Statement st = con.createStatement();) {
+            numCol = st.executeUpdate(sql);
+            return numCol;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return -1;
+    }
+
+    public int getNumCol() {
+        return numCol;
+    }
+}
